6492ceae3ef2ace84aec4169b90caf6b4ba460d6
Ish Tahir <tahirismaeel@gmail.com>
2020-01-09 14:11:32 -0600

    Changed name of component

---
+++ client/components/All6ATeamsView.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Teams6A = props => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th colSpan="4">6A Classification (AAAAAA)</th>
+        </tr>
+        <tr>
+          <th>School</th>
+          <th>Mascot</th>
+          <th>District</th>
+          <th>Enrollment</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.teams.map((team, i) => {
+          return (
+            <tr key={i}>
+              <td className="school">{team.city ? `${team.city} ${team.school}` : `${team.school}`}</td>
+              <td className="mascot">{team.mascot}</td>
+              <td className="district num">{team.district}</td>
+              <td className="enrollment num">{team.enrollment}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default Teams6A;
