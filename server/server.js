ac7e471a1f9658294013fb2ccc0bbea511e5f485
Ish Tahir <tahirismaeel@gmail.com>
2020-01-09 08:10:55 -0600

    Added get route to test mongo

--- server/server.js
+++ server/server.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const mongo = require('./mongo.js');
 
 const app = express();
 const port = process.env.PORT || 4545;
 
+app.use(express.json());
 app.use(express.static('public'));
+
+app.get('/oneteam', (req, res) => {
+  const school = req.query.school;
+  mongo.findOne({ school }, (err, item) => {
+    if (err) {
+      res.send(err);
+      return;
+    }
+    res.send(item);
+  });
+});
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
