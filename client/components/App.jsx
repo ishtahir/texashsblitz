c765073cb8eb86fae8a5b0592117a9b2063fcd35
Ish Tahir <tahirismaeel@gmail.com>
2020-01-09 10:55:05 -0600

    Added sort function

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -19,7 +19,15 @@ class App extends Component {
     axios.get('/all').then(res => this.setState({ teams6A: res.data }));
   }
 
+  sort(criteria) {
+    let sorted;
+    if (criteria === 'district') {
+      sorted = this.state.teams6A.sort((a, b) => a.district - b.district);
+    }
+  }
+
   render() {
+    this.sort('district');
     return (
       <>
         <h1 className="headline">Welcome to Texas HS BLiTZ</h1>
