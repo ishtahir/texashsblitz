0fa159eb75c7997aeb339341d78ba4afbe0baff7
Ish Tahir <tahirismaeel@gmail.com>
2020-01-09 08:38:38 -0600

    Added state, componentDidMount to get all teams

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      teams6A: []
+    };
+  }
+
+  componentDidMount() {
+    this.getAllTeams();
+  }
+
+  getAllTeams() {
+    axios.get('/all').then(res => this.setState({ teams6A: res.data }));
+  }
+
   render() {
-    return <h1>Welcome</h1>;
+    return <h1>Welcome to Texas HS BLiTZ</h1>;
   }
 }
 
