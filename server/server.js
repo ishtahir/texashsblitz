ff28948954df8b7aafd042442b6180ea35963a4d
Ish Tahir <tahirismaeel@gmail.com>
2020-01-09 12:26:03 -0600

    Added cors

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongo = require('./mongo.js');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 4545;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
