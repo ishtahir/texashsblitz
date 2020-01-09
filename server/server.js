a132f01443d2344dcfb7d1817dbb33d5114d953a
Ish Tahir <tahirismaeel@gmail.com>
2020-01-08 22:11:43 -0600

    Adding starter files

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 4545;
+
+app.use(express.static('public'));
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
