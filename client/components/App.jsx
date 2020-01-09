3aa0981dd2a7b9df04e02b930cb4a1adfde4b84c
Ish Tahir <tahirismaeel@gmail.com>
2020-01-09 14:12:04 -0600

    Added districts in state

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import Teams6A from './Teams6A';
+import All6ATeamsView from './All6ATeamsView';
 import Navbar from './Navbar';
+import DistrictView from './DistrictView';
 import axios from 'axios';
 
 class App extends Component {
@@ -8,7 +9,9 @@ class App extends Component {
     super();
 
     this.state = {
-      teams6A: []
+      teams6A: [],
+      view: 'all',
+      districts: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]
     };
   }
 
@@ -20,6 +23,10 @@ class App extends Component {
     axios.get('http://localhost:4545/all').then(res => this.setState({ teams6A: res.data }));
   }
 
+  changeView(view) {
+    this.setState({ view });
+  }
+
   sort(criteria) {
     let sorted;
     if (criteria === 'district') {
@@ -27,12 +34,22 @@ class App extends Component {
     }
   }
 
+  renderView() {
+    if (this.state.view === 'all') {
+      return <All6ATeamsView teams={this.state.teams6A} />;
+    } else if (this.state.view === 'district') {
+      return <DistrictView districts={this.state.districts} teams={this.state.teams6A} />;
+    } else if (this.state.view === 'champions') {
+      return <StateAppearancesView />;
+    }
+  }
+
   render() {
     this.sort('district');
     return (
       <>
-        <Navbar />
-        <Teams6A teams={this.state.teams6A} />
+        <Navbar changeView={this.changeView.bind(this)} />
+        {this.renderView()}
       </>
     );
   }
