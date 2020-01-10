930dc535b75d36d30a5aa4394dcdd76e156507aa
Ish Tahir <tahirismaeel@gmail.com>
2020-01-09 20:25:45 -0600

    Added all teams tab

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -7,10 +7,15 @@ const Navbar = props => {
         <img src="../../public/american-football.svg" alt="Football" className="football-icon" /> Texas HS <span className="blitz">Blitz</span>
       </h1>
       <ul className="nav-items">
+        <li className="nav-item" onClick={() => props.changeView('all')}>
+          All Teams
+        </li>
         <li className="nav-item" onClick={() => props.changeView('district')}>
           Districts
         </li>
-        <li className="nav-item">State Championship Appearances</li>
+        <li className="nav-item" onClick={() => props.changeView('champions')}>
+          State Championship Appearances
+        </li>
         <input type="text" className="search" placeholder="&#x1F50D; City, School, Mascot" />
       </ul>
     </nav>
