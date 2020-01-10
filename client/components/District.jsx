b0a5b24bd2d5f63976b7886378994bc1e802efec
Ish Tahir <tahirismaeel@gmail.com>
2020-01-09 18:01:41 -0600

    Added inline styles for each table row

--- client/components/District.jsx
+++ client/components/District.jsx
@@ -5,7 +5,9 @@ const District = props => {
     <table>
       <thead>
         <tr>
-          <th colSpan="3">Class 6A District {props.district}</th>
+          <th colSpan="3" className="table-header">
+            Class 6A District {props.district}
+          </th>
         </tr>
         <tr>
           <th>School</th>
@@ -14,9 +16,9 @@ const District = props => {
         </tr>
       </thead>
       <tbody>
-        {props.teams.map(team => {
+        {props.teams.map((team, i) => {
           return (
-            <tr>
+            <tr key={`${team}${i}`} style={{ background: team.colors[0], color: team.colors[1] }}>
               <td className="school">{team.city ? `${team.city} ${team.school}` : `${team.school}`}</td>
               <td className="mascot">{team.mascot}</td>
               <td className="enrollment num">{team.enrollment}</td>
