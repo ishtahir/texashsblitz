3b71093310621e8e97b3b84e67628233984d46a7
Ish Tahir <tahirismaeel@gmail.com>
2020-01-09 20:25:16 -0600

    Added key prop

--- client/components/DistrictView.jsx
+++ client/components/DistrictView.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import District from './District';
 
 const DistrictView = props => {
-  return props.districts.map(district => <District district={district} teams={props.teams.filter(team => team.district === district)} />);
+  return props.districts.map(district => (
+    <District key={`dist${district}`} district={district} teams={props.teams.filter(team => team.district === district)} />
+  ));
 };
 
 export default DistrictView;
