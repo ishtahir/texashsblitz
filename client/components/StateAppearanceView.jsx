f1bdf6b649c9618e29003f3c7b144179c76ca0dc
Ish Tahir <tahirismaeel@gmail.com>
2020-01-09 20:26:18 -0600

    Completed the table for this component

--- client/components/StateAppearanceView.jsx
+++ client/components/StateAppearanceView.jsx
@@ -1,5 +1,33 @@
 import React from 'react';
 
-const StateAppearanceView = props => {};
+const StateAppearanceView = props => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th colSpan="4" className="table-header">
+            UIL State Championship Appearances
+          </th>
+        </tr>
+        <tr>
+          <th>School</th>
+          <th>Appearances</th>
+          <th>Years</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.teams.map((team, i) => {
+          return (
+            <tr key={`${team}${i}`} style={{ background: team.colors[0], color: team.colors[1] }}>
+              <td className="school">{team.city ? `${team.city} ${team.school} ${team.mascot}` : `${team.school} ${team.mascot}`}</td>
+              <td className="appearances num">{team.stateAppearences.length}</td>
+              <td className="years">{team.stateAppearences.sort((a, b) => a - b).join(', ')}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
 
 export default StateAppearanceView;
