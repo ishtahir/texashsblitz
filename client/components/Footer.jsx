76f7e5e950dfad3de50b8f76c27dbdae325b364a
Ish Tahir <tahirismaeel@gmail.com>
2020-01-10 08:30:47 -0600

    Added Footer

---
+++ client/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = props => {
+  return (
+    <footer className="footer">
+      <p>Copyright &copy; 2020. All Rights Reserved.</p>
+      <p>Created with ❤️ by Ish Tahir for MVP.</p>
+    </footer>
+  );
+};
+
+export default Footer;
