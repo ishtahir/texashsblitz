b7df3f8514f495df180438a770ee38c740042ac0
Ish Tahir <tahirismaeel@gmail.com>
2020-02-14 10:53:06 -0600

    Applying styles dynamically with classes

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -3,71 +3,44 @@ import React from 'react';
 const Navbar = props => {
   return (
     <nav className="navbar" style={{ position: 'sticky', top: 0 }}>
-      <div className="nav-icon" onClick={evt => hamburger(evt, props)}>
-        <div className="line line1"></div>
-        <div className="line line2"></div>
-        <div className="line line3"></div>
+      <div className="hamburger-container" onClick={props.handleHamburger} style={{ top: `${props.hamburgerClicked ? '5%' : '15%'}` }}>
+        <div className={props.hamburgerClicked ? 'line line1' : 'line'}></div>
+        <div className={props.hamburgerClicked ? 'line line2' : 'line'}></div>
+        <div className={props.hamburgerClicked ? 'line line3' : 'line'}></div>
       </div>
-      <h1 className="logo">
-        <img src="../../american-football.svg" alt="Football" className="football-icon" /> Texas HS <span className="blitz">Blitz</span>
-      </h1>
       <ul className="nav-items">
-        <li className={`${props.view === 'all' ? 'current' : ''} nav-item`} onClick={() => props.changeView('all')}>
+        <h1 className="texashs">
+          <div className="logo-container">
+            <img src="../../american-football.svg" alt="Football" className="football-icon" /> Texas HS <span className="blitz">Blitz</span>
+          </div>
+        </h1>
+        <li
+          className={`${props.view === 'all' ? 'current' : ''} nav-item ${props.hamburgerClicked ? 'block' : 'none'}`}
+          onClick={() => props.changeView('all')}
+        >
           Classes
         </li>
-        <li className={`${props.view === 'district' ? 'current' : ''} nav-item`} onClick={() => props.changeView('district')}>
+        <li
+          className={`${props.view === 'district' ? 'current' : ''} nav-item ${props.hamburgerClicked ? 'block' : 'none'}`}
+          onClick={() => props.changeView('district')}
+        >
           Districts
         </li>
-        <li className={`${props.view === 'enroll' ? 'current' : ''} nav-item`} onClick={() => props.changeView('enroll')}>
+        <li
+          className={`${props.view === 'enroll' ? 'current' : ''} nav-item ${props.hamburgerClicked ? 'block' : 'none'}`}
+          onClick={() => props.changeView('enroll')}
+        >
           Enrollment
         </li>
-        <li className={`${props.view === 'champions' ? 'current' : ''} nav-item`} onClick={() => props.changeView('champions')}>
+        <li
+          className={`${props.view === 'champions' ? 'current' : ''} nav-item ${props.hamburgerClicked ? 'block' : 'none'}`}
+          onClick={() => props.changeView('champions')}
+        >
           State Appearances
         </li>
       </ul>
     </nav>
   );
-};
-
-const hamburger = (evt, props) => {
-  if (evt.target.className === 'nav-icon') {
-    if (evt.target.querySelector('.line1').style.transform === 'translateY(12px) rotate(135deg)') {
-      evt.target.querySelector('.line1').style.transform = 'translateY(0) rotate(0)';
-      evt.target.querySelector('.line2').style.transform = 'translateY(0) rotate(0)';
-      evt.target.querySelector('.line3').style.transform = 'translateY(0) rotate(0)';
-      let navitems = document.querySelectorAll('.nav-item');
-      for (let i = 0; i < navitems.length; i++) {
-        navitems[i].style.display = 'none';
-      }
-    } else {
-      evt.target.querySelector('.line1').style.transform = 'translateY(12px) rotate(135deg)';
-      evt.target.querySelector('.line2').style.transform = 'scale(0)';
-      evt.target.querySelector('.line3').style.transform = 'translateY(-12px) rotate(-135deg)';
-      let navitems = document.querySelectorAll('.nav-item');
-      for (let i = 0; i < navitems.length; i++) {
-        navitems[i].style.display = 'block';
-      }
-    }
-  } else {
-    if (evt.target.parentNode.querySelector('.line1').style.transform === 'translateY(12px) rotate(135deg)') {
-      evt.target.parentNode.querySelector('.line1').style.transform = 'translateY(0) rotate(0)';
-      evt.target.parentNode.querySelector('.line2').style.transform = 'translateY(0) rotate(0)';
-      evt.target.parentNode.querySelector('.line3').style.transform = 'translateY(0) rotate(0)';
-      let navitems = document.querySelectorAll('.nav-item');
-      for (let i = 0; i < navitems.length; i++) {
-        navitems[i].style.display = 'none';
-      }
-    } else {
-      evt.target.parentNode.querySelector('.line1').style.transform = 'translateY(12px) rotate(135deg)';
-      evt.target.parentNode.querySelector('.line2').style.transform = 'scale(0)';
-      evt.target.parentNode.querySelector('.line3').style.transform = 'translateY(-12px) rotate(-135deg)';
-      let navitems = document.querySelectorAll('.nav-item');
-      for (let i = 0; i < navitems.length; i++) {
-        navitems[i].style.display = 'block';
-      }
-    }
-  }
-  props.handleHamburger();
 };
 
 export default Navbar;
