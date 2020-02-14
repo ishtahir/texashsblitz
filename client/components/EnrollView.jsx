6755c599ce9c5655e75e610478da15f5126c2e4a
Ish Tahir <tahirismaeel@gmail.com>
2020-02-14 12:05:16 -0600

    Removed class from th added to td

--- client/components/EnrollView.jsx
+++ client/components/EnrollView.jsx
@@ -5,23 +5,21 @@ const EnrollView = props => {
     <table>
       <thead>
         <tr>
-          <th colSpan="4" className="table-header">
+          <th colSpan="2" className="table-header">
             Class 6A by Enrollment
           </th>
         </tr>
         <tr>
-          <th className="school">School</th>
-          <th className="mascot">Mascot</th>
-          <th className="enrollment">Enrollment</th>
+          <th>School</th>
+          <th>Enrollment</th>
         </tr>
       </thead>
       <tbody>
         {props.teams.map((team, i) => {
           return (
             <tr key={i} style={{ background: team.colors[0], color: team.colors[1] }}>
-              <td className="school">{team.city ? `${team.city} ${team.school}` : `${team.school}`}</td>
-              <td className="mascot">{team.mascot}</td>
-              <td className="enrollment num">{team.enrollment}</td>
+              <td className="school-mascot">{team.city ? `${team.city} ${team.school} ${team.mascot}` : `${team.school} ${team.mascot}`}</td>
+              <td className="enroll">{team.enrollment}</td>
             </tr>
           );
         })}
