6108dcbfbae35ee8516cfe05b90249635c9d0f7d
Ish Tahir <tahirismaeel@gmail.com>
2020-02-15 11:33:12 -0600

    Removed some comments, cleaned up code

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
-// const mongo = require('./mongo.js');
+const mongo = require('./mongo.js');
 
 const app = express();
 const port = process.env.PORT || 4545;
@@ -10,26 +10,15 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-// app.get('/oneteam', (req, res) => {
-//   const school = req.query.school;
-//   mongo.findOne({ school }, (err, item) => {
-//     if (err) {
-//       res.send(err);
-//       return;
-//     }
-//     res.send(item);
-//   });
-// });
-
-// app.get('/all', (req, res) => {
-//   mongo.find({}).toArray((err, items) => {
-//     if (err) {
-//       res.send(err);
-//     } else {
-//       res.send(items);
-//     }
-//   });
-// });
+app.get('/all', (req, res) => {
+  mongo.find({}).toArray((err, items) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(items);
+    }
+  });
+});
 
 app.get('/load', (req, res) => {
   axios
