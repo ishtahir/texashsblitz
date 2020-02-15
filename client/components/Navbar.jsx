d0bb3c609361e39795664bf7d6cea2df27048bd0
Ish Tahir <tahirismaeel@gmail.com>
2020-02-15 11:30:32 -0600

    Moved logo-container outside the logo elems

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 
 const Navbar = props => {
   return (
-    <nav className="navbar" style={{ position: 'sticky', top: 0 }}>
+    <nav className="navbar">
       <div className="hamburger-container" onClick={props.handleHamburger} style={{ top: `${props.hamburgerClicked ? '5%' : '15%'}` }}>
         <div className={props.hamburgerClicked ? 'line line1' : 'line'}></div>
         <div className={props.hamburgerClicked ? 'line line2' : 'line'}></div>
         <div className={props.hamburgerClicked ? 'line line3' : 'line'}></div>
       </div>
       <ul className="nav-items">
-        <h1 className="texashs">
-          <div className="logo-container">
+        <div className="logo-container">
+          <h1 className="texashs">
             <img src="../../american-football.svg" alt="Football" className="football-icon" /> Texas HS <span className="blitz">Blitz</span>
-          </div>
-        </h1>
+          </h1>
+        </div>
         <li
           className={`${props.view === 'all' ? 'current' : ''} nav-item ${props.hamburgerClicked ? 'block' : 'none'}`}
           onClick={() => props.changeView('all')}
