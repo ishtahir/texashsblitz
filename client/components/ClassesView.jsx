ddfd70947cf2bfe2602223f9b868ee3cea4758a1
Ish Tahir <tahirismaeel@gmail.com>
2020-02-22 12:10:26 -0600

    Dynamic rendering of columns
    - If all teams selected in mobile school/mascot are combined in one
    column and second column becomes classification/division
    - Otherwise it shows school and mascot respectively

--- client/components/ClassesView.jsx
+++ client/components/ClassesView.jsx
@@ -12,19 +12,29 @@ const ClassesView = props => {
           </th>
         </tr>
         <tr>
-          <th>School</th>
-          <th>Mascot</th>
-          <th style={{ display: `${props.isDesktop ? 'table-cell' : 'none'}` }}>{props.currentClass > 6 ? 'Class' : 'Enrollment'}</th>
+          <th>{props.currentClass > 6 ? 'School/Mascot' : 'School'}</th>
+          <th style={{ display: props.currentClass > 6 ? 'none' : 'table-cell' }}>Mascot</th>
+          <th style={{ display: `${props.isDesktop || props.currentClass > 6 ? 'table-cell' : 'none'}` }}>
+            {props.currentClass > 6 ? 'Class' : 'Enrollment'}
+          </th>
         </tr>
       </thead>
       <tbody>
         {props.teams.map((team, i) => {
           return (
             <tr key={i} style={{ background: team.colors[0], color: team.colors[1] }}>
-              <td className="school">{team.city ? `${team.city} ${team.school}` : `${team.school}`}</td>
-              <td className="mascot">{team.mascot}</td>
+              <td className="school">
+                {props.currentClass > 6
+                  ? team.city
+                    ? `${team.city} ${team.school} ${team.mascot}`
+                    : `${team.school} ${team.mascot}`
+                  : `${team.school}`}
+              </td>
+              <td className="mascot" style={{ display: props.currentClass > 6 ? 'none' : 'table-cell' }}>
+                {team.mascot}
+              </td>
               {props.currentClass > 6 ? (
-                <td className="class-all" style={{ display: `${props.isDesktop ? 'table-cell' : 'none'}` }}>
+                <td className="class-all" style={{ display: `${props.isDesktop || props.currentClass > 6 ? 'table-cell' : 'none'}` }}>
                   {team.class === 6 ? `${team.class}A` : `${team.class}A D${team.division}`}
                 </td>
               ) : (
