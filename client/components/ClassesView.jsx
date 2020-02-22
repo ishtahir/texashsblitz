240f9a506c95ae7a0d7ba9fe746629fa3576c4fa
Ish Tahir <tahirismaeel@gmail.com>
2020-02-22 11:37:03 -0600

    Conditional rendering of last column
    - Last column will be either enrollment or classes for all teams
    - the td tag also will render differently depending on currentClass

--- client/components/ClassesView.jsx
+++ client/components/ClassesView.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 
 const ClassesView = props => {
   let header = `${props.currentClass}A Classification (${'A'.repeat(props.currentClass)})`;
+  let headerAll = 'All Teams (All Classifications)';
   return (
     <table>
       <thead>
         <tr>
           <th colSpan="3" className="table-header">
-            {props.currentClass === 6 ? header : `${header} D${props.currentDivision}`}
+            {props.currentClass > 6 ? headerAll : props.currentClass === 6 ? header : `${header} D${props.currentDivision}`}
           </th>
         </tr>
         <tr>
           <th>School</th>
           <th>Mascot</th>
-          <th style={{ display: `${props.isDesktop ? 'table-cell' : 'none'}` }}>Enrollment</th>
+          <th style={{ display: `${props.isDesktop ? 'table-cell' : 'none'}` }}>{props.currentClass > 6 ? 'Class' : 'Enrollment'}</th>
         </tr>
       </thead>
       <tbody>
@@ -22,9 +23,15 @@ const ClassesView = props => {
             <tr key={i} style={{ background: team.colors[0], color: team.colors[1] }}>
               <td className="school">{team.city ? `${team.city} ${team.school}` : `${team.school}`}</td>
               <td className="mascot">{team.mascot}</td>
-              <td className="enroll" style={{ display: `${props.isDesktop ? 'table-cell' : 'none'}` }}>
-                {team.enrollment}
-              </td>
+              {props.currentClass > 6 ? (
+                <td className="class-all" style={{ display: `${props.isDesktop ? 'table-cell' : 'none'}` }}>
+                  {team.class === 6 ? `${team.class}A` : `${team.class}A D${team.division}`}
+                </td>
+              ) : (
+                <td className="enroll" style={{ display: `${props.isDesktop ? 'table-cell' : 'none'}` }}>
+                  {team.enrollment}
+                </td>
+              )}
             </tr>
           );
         })}
