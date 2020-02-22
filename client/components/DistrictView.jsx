c81de05510026fc81f8a9e978fd5cf7491307076
Ish Tahir <tahirismaeel@gmail.com>
2020-02-22 14:45:55 -0600

    Renders message is you go to district view in all teams

--- client/components/DistrictView.jsx
+++ client/components/DistrictView.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 import District from './District';
 
 const DistrictView = props => {
-  return props.districts.map(district => (
-    <District
-      key={`dist${district}`}
-      district={district}
-      teams={props.teams.filter(team => team.district === district)}
-      isDesktop={props.isDesktop}
-      currentClass={props.currentClass}
-      currentDivision={props.currentDivision}
-    />
-  ));
+  return props.currentClass > 6 ? (
+    <p className="text-desc">Please change to a specific class.</p>
+  ) : (
+    props.districts.map(district => (
+      <District
+        key={`dist${district}`}
+        district={district}
+        teams={props.teams.filter(team => team.district === district)}
+        isDesktop={props.isDesktop}
+        currentClass={props.currentClass}
+        currentDivision={props.currentDivision}
+      />
+    ))
+  );
 };
 
 export default DistrictView;
