1b3ed2dadef88cec3fa31975c95dc1d3766e5e46
Ish Tahir <tahirismaeel@gmail.com>
2020-02-25 12:49:06 -0600

    Dynamic route
    - Created variable to pick default route based on if work is done
    locally or deployed
    - Just have to comment the require mongo and uncomment the other mongo

--- server/server.js
+++ server/server.js
@@ -1,27 +1,34 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
-// const urlFile = require('./url.js');
-// const mongo = require('./mongo.js');
+const urlFile = require('./url.js');
+const mongo = require('./mongo.js');
+// let mongo;
 
 const app = express();
 const port = process.env.PORT || 4545;
 const url = process.env.API_URL || urlFile;
+const main = mongo ? 'local' : 'api';
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-// app.get('/all', (req, res) => {
-//   mongo.find({}).toArray((err, items) => {
-//     if (err) {
-//       res.send(err);
-//     } else {
-//       res.send(items);
-//     }
-//   });
-// });
-app.get('/load', (req, res) => {
+app.get('/start', (req, res) => {
+  res.send(main);
+});
+
+app.get(`/${main}`, (req, res) => {
+  mongo.find({}).toArray((err, items) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(items);
+    }
+  });
+});
+
+app.get(`/${main}`, (req, res) => {
   axios
     .get(url)
     .then(response => {
