027d5bd8548fe86c611adb2385720a2736ea6da2
Ish Tahir <tahirismaeel@gmail.com>
2020-02-25 19:17:14 -0600

    Reverted back to old axios method

--- server/server.js
+++ server/server.js
@@ -3,34 +3,26 @@ const cors = require('cors');
 const axios = require('axios');
 // const urlFile = require('./url.js');
 // const mongo = require('./mongo.js');
-let mongo;
 
 const app = express();
 const port = process.env.PORT || 4545;
 const url = process.env.API_URL; // || urlFile;
-const main = mongo ? 'local' : 'api';
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/start', (req, res) => {
-  res.send(main);
-});
+// app.get(`/local`, (req, res) => {
+//   mongo.find({}).toArray((err, items) => {
+//     if (err) {
+//       res.send(err);
+//     } else {
+//       res.send(items);
+//     }
+//   });
+// });
 
-app.get(`/${main}`, (req, res) => {
-  mongo.find({}).toArray((err, items) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(items);
-    }
-  });
-});
-
-console.log(url);
-
-app.get(`/${main}`, (req, res) => {
+app.get('/api', (req, res) => {
   axios
     .get(url)
     .then(response => {
