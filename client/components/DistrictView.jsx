5faedd4a8b80fdc7061fcf3f117a39dd6f40842c
Ish Tahir <tahirismaeel@gmail.com>
2020-03-07 03:22:46 -0600

    Destructure props for cleaner look and less code

--- client/components/DistrictView.jsx
+++ client/components/DistrictView.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import District from './District';
 
-const DistrictView = props => {
-  return props.currentClass > 6 ? (
+const DistrictView = ({ currentClass, districts, teams, isDesktop, currentDivision }) => {
+  return currentClass > 6 ? (
     <p className="text-desc change">Please change to a specific classification to see teams</p>
   ) : (
-    props.districts.map(district => (
+    districts.map(district => (
       <District
         key={`dist${district}`}
         district={district}
-        teams={props.teams.filter(team => team.district === district)}
-        isDesktop={props.isDesktop}
-        currentClass={props.currentClass}
-        currentDivision={props.currentDivision}
+        teams={teams.filter(team => team.district === district)}
+        isDesktop={isDesktop}
+        currentClass={currentClass}
+        currentDivision={currentDivision}
       />
     ))
   );
