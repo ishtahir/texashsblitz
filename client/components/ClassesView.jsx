63017b09b36f1de7ccb8b99bd7de7a35664261ed
Ish Tahir <tahirismaeel@gmail.com>
2020-03-07 03:20:38 -0600

    Destructure props for cleaner look and less code

--- client/components/ClassesView.jsx
+++ client/components/ClassesView.jsx
@@ -1,44 +1,42 @@
 import React from 'react';
 
-const ClassesView = props => {
-  let header = `${props.currentClass}A Classification (${'A'.repeat(props.currentClass)})`;
+const ClassesView = ({ currentClass, currentDivision, isDesktop, teams }) => {
+  let header = `${currentClass}A Classification (${'A'.repeat(currentClass)})`;
   let headerAll = 'All Teams (All Classifications)';
   return (
     <table>
       <thead>
         <tr>
           <th colSpan="3" className="table-header">
-            {props.currentClass > 6 ? headerAll : props.currentClass === 6 ? header : `${header} D${props.currentDivision}`}
+            {currentClass > 6 ? headerAll : currentClass === 6 ? header : `${header} D${currentDivision}`}
           </th>
         </tr>
         <tr>
           <th>School</th>
-          <th style={{ display: props.currentClass > 6 ? 'none' : 'table-cell' }}>Mascot</th>
-          <th style={{ display: `${props.isDesktop || props.currentClass > 6 ? 'table-cell' : 'none'}` }}>
-            {props.currentClass > 6 ? 'Class' : 'Enrollment'}
-          </th>
+          <th style={{ display: currentClass > 6 ? 'none' : 'table-cell' }}>Mascot</th>
+          <th style={{ display: `${isDesktop || currentClass > 6 ? 'table-cell' : 'none'}` }}>{currentClass > 6 ? 'Class' : 'Enrollment'}</th>
         </tr>
       </thead>
       <tbody>
-        {props.teams.map((team, i) => {
+        {teams.map((team, i) => {
           return (
             <tr key={i} style={{ background: team.colors[0], color: team.colors[1] }}>
               <td className="school">
-                {props.currentClass > 6
+                {currentClass > 6
                   ? team.city
                     ? `${team.city} ${team.school} ${team.mascot}`
                     : `${team.school} ${team.mascot}`
                   : `${team.city} ${team.school}`}
               </td>
-              <td className="mascot" style={{ display: props.currentClass > 6 ? 'none' : 'table-cell' }}>
+              <td className="mascot" style={{ display: currentClass > 6 ? 'none' : 'table-cell' }}>
                 {team.mascot}
               </td>
-              {props.currentClass > 6 ? (
-                <td className="class-all" style={{ display: `${props.isDesktop || props.currentClass > 6 ? 'table-cell' : 'none'}` }}>
+              {currentClass > 6 ? (
+                <td className="class-all" style={{ display: `${isDesktop || currentClass > 6 ? 'table-cell' : 'none'}` }}>
                   {team.class === 6 ? `${team.class}A` : `${team.class}A D${team.division}`}
                 </td>
               ) : (
-                <td className="enroll" style={{ display: `${props.isDesktop ? 'table-cell' : 'none'}` }}>
+                <td className="enroll" style={{ display: `${isDesktop ? 'table-cell' : 'none'}` }}>
                   {team.enrollment}
                 </td>
               )}
