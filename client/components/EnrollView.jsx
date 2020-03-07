a1bb5fa7f6696c943ef4c958357e683c6c0d3390
Ish Tahir <tahirismaeel@gmail.com>
2020-03-07 03:22:55 -0600

    Destructure props for cleaner look and less code

--- client/components/EnrollView.jsx
+++ client/components/EnrollView.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const EnrollView = props => {
-  let header = `Class ${props.currentClass}A by Enrollment`;
+const EnrollView = ({ currentClass, teams }) => {
+  let header = `Class ${currentClass}A by Enrollment`;
   let headerAll = 'All Teams by Enrollment';
   return (
     <table>
       <thead>
         <tr>
           <th colSpan="2" className="table-header">
-            {props.currentClass > 6 ? headerAll : header}
+            {currentClass > 6 ? headerAll : header}
           </th>
         </tr>
         <tr>
@@ -17,7 +17,7 @@ const EnrollView = props => {
         </tr>
       </thead>
       <tbody>
-        {props.teams.map((team, i) => {
+        {teams.map((team, i) => {
           return (
             <tr key={i} style={{ background: team.colors[0], color: team.colors[1] }}>
               <td className="school-mascot">{team.city ? `${team.city} ${team.school} ${team.mascot}` : `${team.school} ${team.mascot}`}</td>
