c937dde555c260535f90cb12724f1c2c362c16a5
Ish Tahir <tahirismaeel@gmail.com>
2021-01-13 11:41:12 -0600

    Added new state classes for logo colors

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -2,38 +2,60 @@ import React from 'react';
 
 const Navbar = (props) => {
   return (
-    <nav className="navbar">
-      <div className="hamburger-container" onClick={props.handleHamburger} style={{ top: `${props.hamburgerClicked ? '5%' : '15%'}` }}>
+    <nav className='navbar'>
+      <div
+        className='hamburger-container'
+        onClick={props.handleHamburger}
+        style={{ top: `${props.hamburgerClicked ? '5%' : '15%'}` }}
+      >
         <div className={props.hamburgerClicked ? 'line line1' : 'line'}></div>
         <div className={props.hamburgerClicked ? 'line line2' : 'line'}></div>
         <div className={props.hamburgerClicked ? 'line line3' : 'line'}></div>
       </div>
-      <ul className="nav-items">
-        <div className="logo-container">
-          <h1 className="texashs">
-            <img src="../../american-football.svg" alt="Football" className="football-icon" /> TXHSFB <span className="state">STATE</span>
+      <ul className='nav-items'>
+        <div className='logo-container'>
+          <h1 className='texashs'>
+            <img
+              src='../../american-football.svg'
+              alt='Football'
+              className='football-icon'
+            />{' '}
+            TXHSFB{' '}
+            <span className='state'>
+              <span className='logo-st'>ST</span>
+              <span className='logo-a'>A</span>
+              <span className='logo-te'>TE</span>
+            </span>
           </h1>
         </div>
         <li
-          className={`${props.view === 'classes' ? 'current' : ''} nav-item ${props.hamburgerClicked ? 'block' : 'none'}`}
+          className={`${props.view === 'classes' ? 'current' : ''} nav-item ${
+            props.hamburgerClicked ? 'block' : 'none'
+          }`}
           onClick={() => props.handleChangeView('classes')}
         >
           Classes
         </li>
         <li
-          className={`${props.view === 'districts' ? 'current' : ''} nav-item ${props.hamburgerClicked ? 'block' : 'none'}`}
+          className={`${props.view === 'districts' ? 'current' : ''} nav-item ${
+            props.hamburgerClicked ? 'block' : 'none'
+          }`}
           onClick={() => props.handleChangeView('districts')}
         >
           Districts
         </li>
         <li
-          className={`${props.view === 'enroll' ? 'current' : ''} nav-item ${props.hamburgerClicked ? 'block' : 'none'}`}
+          className={`${props.view === 'enroll' ? 'current' : ''} nav-item ${
+            props.hamburgerClicked ? 'block' : 'none'
+          }`}
           onClick={() => props.handleChangeView('enroll')}
         >
           Enrollment
         </li>
         <li
-          className={`${props.view === 'appearances' ? 'current' : ''} nav-item ${props.hamburgerClicked ? 'block' : 'none'}`}
+          className={`${
+            props.view === 'appearances' ? 'current' : ''
+          } nav-item ${props.hamburgerClicked ? 'block' : 'none'}`}
           onClick={() => props.handleChangeView('appearances')}
         >
           State Appearances
